1bbcb5ed2e460addf5df922f1a5d4e5a7eea72aa
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-20 17:43:13 +0200

    [UPD] add .trim to searchMovies control

--- src/pages/Cinema.jsx
+++ src/pages/Cinema.jsx
@@ -10,7 +10,7 @@ const Cinema = () => {
     const myApi = "322517280b230da9724d683b9cf1693e";
 
     const searchMovies = (e) => {
-        if (query === "") {
+        if (query.trim() === "") {
             return;
         };
 
