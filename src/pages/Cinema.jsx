b772a7c6eb7aeb2e7f12f6c55e67e91cd8e28a01
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-20 17:31:10 +0200

    [UPD] add ternary operator in p

--- src/pages/Cinema.jsx
+++ src/pages/Cinema.jsx
@@ -8,9 +8,9 @@ const Cinema = () => {
     const [query, setQuery] = useState("");
 
     const myApi = "322517280b230da9724d683b9cf1693e";
-    
+
     const searchMovies = (e) => {
-        if (query === ""){
+        if (query === "") {
             return;
         };
 
@@ -22,11 +22,11 @@ const Cinema = () => {
                     api_key: myApi,
                     query: query,
                 },
-            }).then((resp) =>{
+            }).then((resp) => {
                 setMovies(resp.data.results);
             });
-        };
-    
+    };
+
     const showFlags = (languageMovies) => {
         const flags = {
             it: "fi fi-it",
@@ -34,7 +34,7 @@ const Cinema = () => {
             jp: "fi fi-jp",
             es: "fi fi-es",
         };
-        
+
         return flags[languageMovies] ? flags[languageMovies] : languageMovies;
     };
 
@@ -42,42 +42,48 @@ const Cinema = () => {
     //     searchMovies();
     // }, []);
 
-  return (
-    <>
-        <header>
-            <div>
-                <h1>BOOLFIX</h1>
-                <form action="">
-                    <div>
-                        <input 
-                            type="text"
-                            placeholder="Cerca un film..."
-                            value={query}
-                            onChange={(e) => setQuery(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <button onClick={searchMovies}>
-                            Cerca
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </header>
-        <main>
-            <div>
-                {movies.map((movie) => (
-                    <div key={movie.id}>
-                        <h2>{movie.title}</h2>
-                        <p>Titolo originale: {movie.original_title}</p>
-                        <p className={showFlags(movie.original_language)}></p>
-                        <p>Voto: {movie.vote_average}</p>
-                    </div>
-                ))}
-            </div>
-        </main>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <div>
+                    <h1>BOOLFIX</h1>
+                    <form action="">
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Cerca un film..."
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <button onClick={searchMovies}>
+                                Cerca
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </header>
+            <main>
+                <div>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <h2>{movie.title}</h2>
+                            <p>Titolo originale: {movie.original_title}</p>
+                            <p>
+                                {showFlags(movie.original_language).includes("fi") ? (
+                                    <span className={showFlags(movie.original_language)}></span>
+                                ) : (
+                                    <span>{movie.original_language}</span>
+                                )}
+                            </p>
+                            <p>Voto: {movie.vote_average}</p>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default Cinema
