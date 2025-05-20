9c5eca0c8c4a372bd0c0b8e5a79832c9b985da4d
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-20 12:52:52 +0200

    [UPD] create Cinema page form films and tv series

---
+++ src/pages/Cinema.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react"
+import { NavLink } from "react-router-dom"
+
+import axios from "axios"
+
+const Cinema = () => {
+  return (
+    <div>Cinema</div>
+  )
+}
+
+export default Cinema
