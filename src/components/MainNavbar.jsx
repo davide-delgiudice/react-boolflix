2feca7b2cb3e75c53de2211e8ead0f5098ff2982
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-20 12:37:09 +0200

    [UPD] create MainNavbar component

---
+++ src/components/MainNavbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom"
+
+const MainNavbar = () => {
+  return (
+    <nav>
+        <div>
+            <form action="">
+                <div>
+                    <label htmlFor=""></label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <button>
+                        Cerca
+                    </button>
+                </div>
+            </form>
+        </div>
+    </nav>
+  )
+}
+
+export default MainNavbar
