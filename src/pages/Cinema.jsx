d81a67d77573cb08bcbe5e10706c9fbc6d4f3c97
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-21 14:08:00 +0200

    [FIX] removed html tags and create dinamic key for stars

--- src/pages/Cinema.jsx
+++ src/pages/Cinema.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import axios from "axios"
 
@@ -53,10 +53,14 @@ const Cinema = () => {
         return flags[languageMovies] ? flags[languageMovies] : languageMovies;
     };
 
-    const starVote = (vote) =>{
+    const starVote = (vote) => {
         const stars = Math.ceil(vote/2); 
-        const fullStars = Array(stars).fill(<i className="fa-solid fa-star"></i>);
-        const emptyStars = Array(5 - stars).fill(<i className="fa-regular fa-star"></i>);
+        const fullStars = Array.from({ length: stars }, (elem, index) => (
+            <i key={`full-${index}`} className="fa-solid fa-star"></i>
+        ));
+        const emptyStars = Array.from({ length: 5 - stars }, (elem, index) => (
+            <i key={`empty-${index}`} className="fa-regular fa-star"></i>
+        ));
         
         return [...fullStars, ...emptyStars];
     };
@@ -89,51 +93,41 @@ const Cinema = () => {
                 </div>
             </header>
             <main>
-                {/* film */}
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-6 col-lg-4">
-                            {movies.map((movie) => (
-                                <div key={`movie-${movie.id}`}>
-                                    <img src={`${imgUrl}${movie.poster_path}`} alt={movie.title} />
-                                    <h2>{movie.title}</h2>
-                                    <p>Titolo originale: {movie.original_title}</p>
-                                    <p>
-                                        Lingua originale:
-                                        {showFlags(movie.original_language).includes("fi") ? (
-                                            <span className={showFlags(movie.original_language)}></span>
-                                        ) : (
-                                            <span>{movie.original_language}</span>
-                                        )}
-                                    </p>
-                                    <p>Voto: {starVote(movie.vote_average)}</p>
-                                </div>
-                            ))}
+                <div>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <img src={`${imgUrl}${movie.poster_path}`} alt={movie.title} />
+                            <h2>{movie.title}</h2>
+                            <p>Titolo originale: {movie.original_title}</p>
+                            <p>
+                                Lingua originale:
+                                {showFlags(movie.original_language).includes("fi") ? (
+                                    <span className={showFlags(movie.original_language)}></span>
+                                ) : (
+                                    <span>{movie.original_language}</span>
+                                )}
+                            </p>
+                            <p>Voto: {starVote(movie.vote_average)}</p>
                         </div>
-                    </div>
+                    ))}
                 </div>
-                {/* serie tv */}
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-6 col-4">
-                            {series.map((serie) => (
-                                <div key={`serie-${serie.id}`}>
-                                    <img src={`${imgUrl}${serie.poster_path}`} alt={serie.title} />
-                                    <h2>{serie.name}</h2>
-                                    <p>Titolo originale: {serie.original_name}</p>
-                                    <p>
-                                        Lingua originale:
-                                        {showFlags(serie.original_language).includes("fi") ? (
-                                            <span className={showFlags(serie.original_language)}></span>
-                                        ) : (
-                                            <span>{serie.original_language}</span>
-                                        )}
-                                    </p>
-                                    <p>Voto: {starVote(serie.vote_average)}</p>
-                                </div>
-                            ))}
+                <div>
+                    {series.map((serie) => (
+                        <div key={serie.id}>
+                            <img src={`${imgUrl}${serie.poster_path}`} alt={serie.title} />
+                            <h2>{serie.name}</h2>
+                            <p>Titolo originale: {serie.original_name}</p>
+                            <p>
+                                Lingua originale:
+                                {showFlags(serie.original_language).includes("fi") ? (
+                                    <span className={showFlags(serie.original_language)}></span>
+                                ) : (
+                                    <span>{serie.original_language}</span>
+                                )}
+                            </p>
+                            <p>Voto: {starVote(serie.vote_average)}</p>
                         </div>
-                    </div>
+                    ))}
                 </div>
             </main>
         </>
